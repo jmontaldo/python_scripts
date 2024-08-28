f3c1b26e3e79e2d26449ae97a8543950578a57dd
Juan Montaldo <juanjosemontaldo@hotmail.com>
2024-08-28 16:05:12 -0300

    fixed issue

--- scrap/scrap.py
+++ scrap/scrap.py
@@ -22,13 +22,14 @@ text = str(elements[0].text_content())
 cad = text.split("\n")
 for item in cad:
     if (item == '' or item == "LyngSat Stream"):
-        cad.remove(item)
+        #cad.remove(item)
+        pass
     else:
         item_list.append(item)
-parameters = item_list[39:-35:1]
+parameters = item_list[41:-35:1]
 for parameter in parameters:
     funct_list.append(parameter)
-    if (parameter == "HD" or parameter == "SD"):
+    if (parameter == "HD" or parameter == "SD" or parameter == "A24"):
         if (len(funct_list)) == 8:
             signal_list.append(parametrizador(funct_list))
             funct_list = []
